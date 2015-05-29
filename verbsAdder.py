ec0f80aee395055ace7d1d74ae227c0d048f723b
cinematikk <progaminer@gmail.com>
2015-05-29 17:21:53 +0200

    Update!!
    Now with easier input.

--- verbsAdder.py
+++ verbsAdder.py
@@ -3,5 +3,16 @@ out_file = open('verbs.txt', 'wt')
 
 Verbs=["aller", "être"]
 
+neu = input("Ein neues Verb:")
+Verbs.append(neu)
+mehrVerben = input("Möchtest du mehr Verben hinzufügen?")
+while mehrVerben == "ja":
+    neu = input("Ein neues Verb:")
+    if neu == "break":
+        mehrVerben = "break"
+    else:
+        Verbs.append(neu)
+
 out_file.write(json.dumps(Verbs))
 out_file.close()
+print("Alle Verben wurden in die verbs.txt gespeichert.")
